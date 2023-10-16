d50eef82d6b65fb1b228873e6f5a3a8fd56c6d63
dosion9 <dosion9ood@gmail.com>
2023-10-16 14:24:38 +0900

    delete 기능 구현

--- firebase.js
+++ firebase.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import {
-  collection,
-  addDoc,
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import {
+  doc,
+  getDoc,
   getDocs,
+  addDoc,
+  deleteDoc,
   orderBy,
   query,
+  collection,
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 // Firebase 구성 정보 설정
@@ -28,13 +29,15 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Post
+const guestbook = document.querySelector("#guestbook");
 const guestbookName = document.querySelector("#guestbookName");
 const guestbookContent = document.querySelector("#guestbookContent");
+const guestbookPassword = document.querySelector("#guestbookPassword");
 const guestbookBtn = document.querySelector("#guestbookPostBtn");
-const guestbook = document.querySelector("#guestbook");
-guestbookBtn.addEventListener("click", async () => {
+guestbookBtn.addEventListener("click", () => {
   const doc = {
     name: guestbookName.value,
+    password: guestbookPassword.value,
     content: guestbookContent.value,
     timeStemp: new Date(),
   };
@@ -43,8 +46,10 @@ guestbookBtn.addEventListener("click", async () => {
     return alert("닉네임을 입력해주세요!");
   } else if (!doc.content) {
     return alert("내용을 입력해주세요!");
+  } else if (!doc.password) {
+    return alert("비밀번호를 입력해주세요!");
   } else {
-    await addDoc(collection(db, "guestbook"), doc).then(() => {
+    addDoc(collection(db, "guestbook"), doc).then(() => {
       alert("작성완료!\n방명록을 남겨주셔서 감사합니다!");
       return location.reload();
     });
@@ -60,16 +65,65 @@ docs.forEach((doc) => {
     name: doc.data().name,
     content: doc.data().content,
     timeStemp: doc.data().timeStemp.toDate(),
+    id: doc.id,
   };
   const temp = `
-    <li class="guestbook__item row">
+    <li class="guestbook__item row" data-id=${DBdata.id}>
     <div class="guestbook__name">${DBdata.name}</div>
     <div class="guestbook__content">${DBdata.content}</div>
     <div class="guestbook__date">${printTime(DBdata.timeStemp)}</div>
+    <button  class="guestbook__btn guestbook__btn-toggle">삭제</button>
+    <div class="guestbook_delete-form  row">
+    <input
+      type="password"
+      class="guestbook__input"
+      placeholder="비밀번호"
+    />
+    <button class="guestbook__btn guestbook__btn-delete">삭제</button>
+    <button class="guestbook__btn guestbook__btn-toggle">취소</button>
+  </div>
   </li>`;
-
   guestbook.insertAdjacentHTML("beforeend", temp);
+
+  const liElement = guestbook.lastElementChild;
+  const toggleBtn = liElement.querySelectorAll(".guestbook__btn-toggle");
+  const deleteBtn = liElement.querySelector(".guestbook__btn-delete");
+  const password = liElement.querySelector("input[type='password']");
+
+  liElement.addEventListener("click", function (e) {
+    // 토글버튼
+    if (e.target === toggleBtn[0] || e.target === toggleBtn[1]) {
+      this.classList.toggle("guestbook__item-active");
+    }
+
+    // 삭제기능
+    if (e.target === deleteBtn) {
+      confirm("해당 방명록을 삭제하겠습니까?")
+        ? deleteItem(DBdata.id, password.value)
+        : null;
+    }
+  });
 });
+
+// Delete
+const deleteItem = async function (docId, pw) {
+  const docRef = doc(db, "guestbook", String(docId));
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const docPw = docSnap.data().password;
+    const checkPw = pw == docPw;
+    console.log(checkPw);
+    if (checkPw) {
+      await deleteDoc(docRef);
+      alert("삭제 완료");
+    } else {
+      alert("비밀번호가 다릅니다.");
+    }
+  } else {
+    alert("해당 문서가 존재하지 않습니다.");
+  }
+};
 
 /**
  * 파이어베이스에 있는 timeStemp를 오늘이면 시:분:초
